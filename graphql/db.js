472f4f386a70efc57bdacc9bad21279d7f734a59
Hae Jun Kim <dan.kimhaejun@gmail.com>
2019-11-12 18:10:41 +0900

    Create Movies graphql

---
+++ graphql/db.js
@@ -0,0 +1,17 @@
+import fetch from "node-fetch";
+
+const API_URL =
+  "https://yts.lt/api/v2/list_movies.json?limit=50&minimum_rating=9";
+
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
+  }
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+  return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies);
+};
